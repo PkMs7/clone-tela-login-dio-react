ea8bf0c4b4c7813225a7ddb36a8e347094e94c7d
PkMs7 <patrickmarques2009@hotmail.com>
2024-11-11 17:41:41 -0300

    feat: :sparkles: Adiciona o pacote react-router-dom e componente de botão à aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+
+import { Home } from './pages/home';
 
 
 function App() {
 
   return (
-    <>
-      <h1>Login dio</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import { Button } from "../../components/Button";
+
+const Home = () => {
+
+    return(
+        <>
+            <h1>Home</h1>
+        </>
+    );
+
+};
+
+export { Home };
