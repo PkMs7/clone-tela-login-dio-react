551eeabf9a2e692dc06db93972be28adc695d515
Patrick Marques <patrickmarques2009@hotmail.com>
2024-11-11 20:58:27 -0300

    feat: :sparkles: Adiciona o Header da aplicação

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,10 +1,13 @@
 import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
 
 const Home = () => {
 
     return(
         <>
-            <h1>Home</h1>
+            <Header />
+            <Button title="Teste1" />
+            <Button variant="secondary" title="Teste2"/>
         </>
     );
 
