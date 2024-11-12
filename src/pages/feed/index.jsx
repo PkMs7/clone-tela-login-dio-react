f68ad75f8cd9660b097f1a12b3ba0c5d4b61f6a2
PkMs7 <patrickmarques2009@hotmail.com>
2024-11-12 15:07:16 -0300

    feat: :sparkles: Adiciona página Feed de usuário logado na aplicação

---
+++ src/pages/feed/index.jsx
@@ -0,0 +1,29 @@
+import { Card } from '../../components/Card';
+import { UserInfo } from '../../components/UserInfo';
+import { Header } from '../../components/Header';
+import { Container, Column, Title, TitleHighlight } from './styles';
+
+const Feed = () => {
+  return (
+    <>
+        <Header autenticado={true}/>
+        <Container>
+            <Column flex={3}>
+                <Title>Feed</Title>
+                <Card />
+                <Card />
+                <Card />
+            </Column>
+            <Column flex={1}>
+              <TitleHighlight> # RANKING 5 TOP DA SEMANA </TitleHighlight>
+                <UserInfo nome="Patrick Marques" image="https://avatars.githubusercontent.com/u/100943409?s=48&v=4=" percentual={25}/>
+                <UserInfo nome="Patrick Marques" image="https://avatars.githubusercontent.com/u/100943409?s=48&v=4=" percentual={65}/>
+                <UserInfo nome="Patrick Marques" image="https://avatars.githubusercontent.com/u/100943409?s=48&v=4=" percentual={45}/>
+                <UserInfo nome="Patrick Marques" image="https://avatars.githubusercontent.com/u/100943409?s=48&v=4=" percentual={72}/>
+            </Column>
+        </Container>
+    </>
+  )
+}
+
+export { Feed }; 
